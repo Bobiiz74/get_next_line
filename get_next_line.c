80254ad7f7bb408f9f741a70ff326659aa43014c
Robin <robin.godtschalck@hotmail.fr>
2022-12-08 16:27:46 +0100

    swag

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rgodtsch <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 19:10:07 by rgodtsch          #+#    #+#             */
-/*   Updated: 2022/12/02 16:15:07 by rgodtsch         ###   ########.fr       */
+/*   Updated: 2022/12/08 15:22:24 by rgodtsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,87 @@
 char	*get_next_line(int fd)
 {
 	static char	*stash;
-	char 		*line;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
 	stash = gnl_read(fd, stash);
 	if (!stash)
 		return (NULL);
 	line = gnl_get_line(stash);
 	stash = gnl_rm_old_line(stash);
 	return (line);
-
-	
 }
 
-char	*gnl_read(int fd, char *res)
+char	*gnl_read(int fd, char *stash)
 {
 	ssize_t	bytes_readed;
 	char	*buffer;
 
+	if (!stash)
+		stash = (char *)ft_calloc(1, sizeof(char));
+	buffer = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!stash || !buffer)
+		return (NULL);
+	bytes_readed = 1;
+	while (bytes_readed > 0)
+	{
+		bytes_readed = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_readed == -1)
+		{
+			free(buffer);
+			free(stash);
+			return (NULL);
+		}
+		buffer[bytes_readed] = 0;
+		stash = ft_gnl_strjoin(stash, buffer);
+		if (ft_gnl_strchr(buffer) != -1)
+			break ;
+	}
+	free(buffer);
+	return (stash);
+}
 
+char	*gnl_get_line(char *buffer)
+{
+	int		i;
+	char	*str;
+
+	if (!buffer[0])
+		return (NULL);
+	i = ft_gnl_strchr(buffer);
+	if (i == -1)
+		i = ft_strlen(buffer);
+	str = ft_strndup(buffer, i);
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+char	*gnl_rm_old_line(char *buffer)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (buffer == NULL)
+		return (ft_gnl_remove_free(buffer));
+	i = ft_gnl_strchr(buffer);
+	if (i == -1)
+		return (ft_gnl_remove_free(buffer));
+	str = (char *)ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+	if (!str)
+		return (ft_gnl_remove_free(buffer));
+	j = 0;
+	while (buffer[++i])
+		str[j++] = buffer[i];
+	str[j] = '\0';
+	free(buffer);
+	return (str);
+}
+
+char	*ft_gnl_remove_free(char *buffer)
+{
+	free(buffer);
+	return (NULL);
 }
---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgodtsch <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 18:12:31 by rgodtsch          #+#    #+#             */
+/*   Updated: 2022/12/08 15:21:43 by rgodtsch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*gnl_read(int fd, char *stash);
+char	*gnl_get_line(char *buffer);
+char	*gnl_rm_old_line(char *buffer);
+void	*ft_calloc(size_t nb_elem, size_t size_elem);
+int		ft_gnl_strchr(char *str);
+char	*ft_gnl_strjoin(char *line, char *buff);
+size_t	ft_strlen(char *str);
+char	*ft_strndup(char *str, size_t end);
+char	*ft_gnl_remove_free(char *buffer);
+
+#endif
